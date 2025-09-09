7a03823bd3e7286f1a42f8fc3953065525fe52cf
laura dominguez <laura.dominguezan@gmail.com>
2025-09-09 14:13:41 -0600

    Feat(main): archivo config

---
+++ app/config.py
@@ -0,0 +1,12 @@
+# app/config.py
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    port: int = 8000
+    max_history: int = 5
+    max_response_time: int = 30
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
